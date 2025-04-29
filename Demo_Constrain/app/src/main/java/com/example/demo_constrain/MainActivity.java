ac5e0829c5f045f1a455ee83a2fe35955fccaa3f
lyng315 <2251061820@e.tlu.edu.vn>
2025-04-30 01:57:43 +0700

    Add Demo_Constrain project

---
+++ Demo_Constrain/app/src/main/java/com/example/demo_constrain/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.demo_constrain;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    //khai báo các biến giao diện
+    EditText edtA, edtB, edtResult;
+    Button btnAdd;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Ánh xạ id cho các biến giao diện
+        edtA = findViewById(R.id.edtA);
+        edtB = findViewById(R.id.edtB);
+        edtResult= findViewById(R.id.edtResult);
+        btnAdd = findViewById(R.id.btnAdd);
+        //Xử lý tương tác với người dùng
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Lấy dữ liệu từ edtA, ép sang kiểu int và gán vào biến A
+                int a = Integer.parseInt(edtA.getText().toString());
+                int b = Integer.parseInt(edtB.getText().toString());
+                int c = a + b;
+                edtResult.setText(c + ""); //Hiển thị kết quả
+            }
+        });
+    }
+}
