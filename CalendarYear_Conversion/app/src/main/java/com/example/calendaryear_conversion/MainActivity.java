17d1249bb62a4a27014fab30b87bc273536fffcc
lyng315 <2251061820@e.tlu.edu.vn>
2025-05-03 19:11:26 +0700

    Add CalendarYear_Conversion project

---
+++ CalendarYear_Conversion/app/src/main/java/com/example/calendaryear_conversion/MainActivity.java
@@ -0,0 +1,106 @@
+package com.example.calendaryear_conversion;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnConvert;
+    EditText edtCelendarYear, edtLunarYear;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnConvert = findViewById(R.id.btnConvert);
+        edtCelendarYear = findViewById(R.id.edtCelendarYear);
+        edtLunarYear = findViewById(R.id.edtLunarYear);
+        btnConvert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int CeleY = Integer.parseInt(edtCelendarYear.getText()+"");
+                String can="", chi="";
+                switch (CeleY%10){
+                    case 0:
+                        can = "Canh";
+                        break;
+                    case 1:
+                        can = "Tân";
+                        break;
+                    case 2:
+                        can = "Nhâm";
+                        break;
+                    case 3:
+                        can = "Quý";
+                        break;
+                    case 4:
+                        can = "Giáp";
+                        break;
+                    case 5:
+                        can = "Ất";
+                        break;
+                    case 6:
+                        can = "Bính";
+                        break;
+                    case 7:
+                        can = "Đinh";
+                        break;
+                    case 8:
+                        can = "Mậu";
+                        break;
+                    case 9:
+                        can = "Kỷ";
+                        break;
+                }
+                switch (CeleY%12){
+                    case 0:
+                        chi = "Thân";
+                        break;
+                    case 1:
+                        chi = "Dậu";
+                        break;
+                    case 2:
+                        chi = "Tuất";
+                        break;
+                    case 3:
+                        chi = "Hợi";
+                        break;
+                    case 4:
+                        chi = "Tý";
+                        break;
+                    case 5:
+                        chi = "Sửu";
+                        break;
+                    case 6:
+                        chi = "Dần";
+                        break;
+                    case 7:
+                        chi = "Mẹo";
+                        break;
+                    case 8:
+                        chi = "Thìn";
+                        break;
+                    case 9:
+                        chi = "Tỵ";
+                        break;
+                    case 10:
+                        chi = "Ngọ";
+                        break;
+                    case 11:
+                        chi = "Mùi";
+                        break;
+
+                }
+                String Result="";
+                Result = can+" "+chi;
+                edtLunarYear.setText(Result);
+            }
+        });
+    }
+}
